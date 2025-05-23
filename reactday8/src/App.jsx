939d7f60cd60cc9ad6066a9baa4465cff3df68d5
jaffersadiqjs <jaffersadiq2697@gmail.com>
2025-05-23 12:52:57 +0530

    Update App.jsx

--- reactday8/src/App.jsx
+++ reactday8/src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import TodoList from './Components/TodoList'
+import TodoList from './Components/Todolist'
 
 function App() {
   const [count, setCount] = useState(0)
